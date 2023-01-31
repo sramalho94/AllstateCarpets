aad7fe02bfb5f24c979d83caf8768a47777c53f1
Stephan D Ramalho <sramalho@fordham.edu>
2023-01-31 11:21:22 -0500

    added utility classes to navbar and changed layout

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -1,15 +1,20 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div className="flex text-white justify-between bg-orange-200 bg-gradient-to-r from-amber-600">
-        <div>
-        <Image src="/../public/aclogo.png" alt="logo" height={90} width={90}/>
-        </div>
-        <div>
-        <Link href="/">Home</Link>
-        </div>
+    <div className="mb-20">
+      <nav className="pb-4 mb-12 flex justify-between bg-orange-200 bg-gradient-to-r from-amber-600">
+        <Image src="/../public/aclogo.png " height={90} width={90} alt="logo" />
+        <ul className="flex items-center">
+          <li>
+            <Link href="/" className="text-red-700 font-bold">
+              {' '}
+              Home
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }

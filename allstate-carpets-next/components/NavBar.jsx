9302b119ae382d64dcb32b6b2822115a4996ed77
Jeff Carroll <jcarr048@gmail.com>
2023-02-01 12:26:25 -0500

    Changed coloring and added border to navbar

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -3,18 +3,18 @@ import Link from 'next/link'
 
 const NavBar = () => {
   return (
-    <div className="mb-20">
-      <nav className="pb-4 mb-12 flex justify-between bg-orange-200 bg-gradient-to-r from-amber-600">
+    <div>
+      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#b97c64] border-b-2 border-[#ac1010]">
         <Image src="/../public/aclogo.png " height={90} width={90} alt="logo" />
         <ul className="flex items-center">
           <li>
-            <Link href="/" className="text-red-700 font-bold">
+            <Link href="/" className="text-[#ac1010] font-bold">
               {' '}
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className='text-red-700 font-bold'>
+            <Link href="/about" className='text-[#ac1010] font-bold'>
                 About Us
             </Link>
           </li>

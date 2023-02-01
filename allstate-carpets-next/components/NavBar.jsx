79f4fb4e370e39558b514571433ddc634d7ccd58
Jeff Carroll <jcarr048@gmail.com>
2023-02-01 12:42:49 -0500

    made logo larger on navbar and added padding

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 const NavBar = () => {
   return (
     <div>
-      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#b97c64] border-b-2 border-[#ac1010]">
-        <Image src="/../public/aclogo.png " height={90} width={90} alt="logo" />
+      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#a4614c] border-b-2 border-[#80241b]">
+        <Image src="/../public/aclogo.png " height={110} width={110} alt="logo" className='pl-3 pt-2'/>
         <ul className="flex items-center">
           <li>
             <Link href="/" className="text-[#ac1010] font-bold">

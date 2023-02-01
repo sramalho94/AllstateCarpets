9d5c8ba3a71ef742fcec07ce06c665a285f2edb2
Jeff Carroll <jcarr048@gmail.com>
2023-02-01 12:42:20 -0500

    altered padding and colors on about page

--- allstate-carpets-next/app/about/page.jsx
+++ allstate-carpets-next/app/about/page.jsx
@@ -3,15 +3,16 @@ import Image from "next/image"
 const About = () => {
   return (
     <div id="aboutus-container" className="bg-[#e3cba3] flex flex-col">
-    <h1 className="font-bold text-center">About Allstate Carpets</h1>
     <div className="flex flex-col md:flex-row">
         <div className="flex-1 mb-1">
+    <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-10">Specialty</h1>
     <p className="text-center max-w-lg text-[#ac1010]">
     Allstate Carpets wants to floor you! We specialize in a wide variety of flooring and interior finishes: Carpet, Carpet Tile, Vinyl Flooring, Wood Flooring, Vinyl Base, Wood Base, Carpet cleaning services, and Window Treatments. From start to finish we will work closely with our client and till they are 100% satisfied.
     </p>
-
+    <br></br>
+    <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-3">About Allstate</h1>
     <p className="text-center max-w-lg text-[#ac1010]">
-    Established in 1971, we are family owned business that has been providing outstanding flooring services to the Washington metropolitan area for over 50 years. Bob Pizzano Sr attended a funeral in Dalton, GA where he met all of his wife's family members who were involved in the flooring business. When he returned home he started ordering all the carpet for his construction business and had it shipped to his home in Falls Church, VA. In September of 1971, Allstate Building and Carpet Supplies, Inc. was established and moved into a warehouse space in Alexandria, VA. Carolyn Pizzano became the owner and ran the business from 1971-1983 when her daughter Lisa Paul graduated from St. Leo college and took over the family business. Now, Lisa and her son Jeremy continue to provide the DMV area with quality flooring products. 
+    Established in 1971, we are family owned business that has been providing outstanding flooring services to the Washington metropolitan area for over 50 years. In September of 1971, Allstate Building and Carpet Supplies, Inc. was established and moved into a warehouse space in Alexandria, VA. Carolyn Pizzano became the owner and ran the business from 1971-1983 when her daughter Lisa Paul graduated from St. Leo college and took over the family business. Now, Lisa and her son Jeremy continue to provide the DMV area with quality flooring products. 
     </p>
     </div>
     <div

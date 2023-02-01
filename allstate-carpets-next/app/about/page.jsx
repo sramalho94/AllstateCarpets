e67345626b816ac29ed22830595ee58db764ffa2
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-01 13:59:01 -0500

    added more styling and skeleton for homepage

--- allstate-carpets-next/app/about/page.jsx
+++ allstate-carpets-next/app/about/page.jsx
@@ -1,44 +1,69 @@
-import Image from "next/image"
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div id="aboutus-container" className="bg-[#e3cba3] flex flex-col">
-    <div className="flex flex-col md:flex-row">
-        <div className="flex-1 mb-1">
-    <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-10">Specialty</h1>
-    <p className="text-center max-w-lg text-[#ac1010]">
-    Allstate Carpets wants to floor you! We specialize in a wide variety of flooring and interior finishes: Carpet, Carpet Tile, Vinyl Flooring, Wood Flooring, Vinyl Base, Wood Base, Carpet cleaning services, and Window Treatments. From start to finish we will work closely with our client and till they are 100% satisfied.
-    </p>
-    <br></br>
-    <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-3">About Allstate</h1>
-    <p className="text-center max-w-lg text-[#ac1010]">
-    Established in 1971, we are family owned business that has been providing outstanding flooring services to the Washington metropolitan area for over 50 years. In September of 1971, Allstate Building and Carpet Supplies, Inc. was established and moved into a warehouse space in Alexandria, VA. Carolyn Pizzano became the owner and ran the business from 1971-1983 when her daughter Lisa Paul graduated from St. Leo college and took over the family business. Now, Lisa and her son Jeremy continue to provide the DMV area with quality flooring products. 
-    </p>
-    </div>
     <div
-      id="staff-photo-container"
-      className="flex flex-col justify-center md:flex-row align-middle my-auto flex-1"
+      id="aboutus-container"
+      className="bg-[#e3cba3] flex flex-col min-h-full justify-start align-middle"
     >
-      <div className="flex flex-col mt-2 p-2 hover:brightness-50">
-        <Image
-          className="rounded-full  shadow-2xl pt-1"
-          src="/../public/LisaPaul.jpeg"
-          alt="picture1"
-          width={120} height={120}
-        />
-        <p>Lisa Paul, President</p>
-      </div>
-      <div className="flex flex-col mt-2 p-2 hover:brightness-50">
-        <Image 
-          className="rounded-full shadow-2xl pt-1"
-          src="/../public/JMP.jpeg"
-          alt="picture1" width={160} height={160}
-        />
-        <p>Jeremy Paul, Vice President</p>
-      </div>
+      <h1 className="text-5xl text-center align-top justify-start pt-20 font-Anton">
+        The AllState Carpet Family
+      </h1>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1 mb-1">
+          <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-10">
+            Specialty
+          </h1>
+          <p className="text-center max-w-lg text-[#ac1010]">
+            Allstate Carpets wants to floor you! We specialize in a wide variety
+            of flooring and interior finishes: Carpet, Carpet Tile, Vinyl
+            Flooring, Wood Flooring, Vinyl Base, Wood Base, Carpet cleaning
+            services, and Window Treatments. From start to finish we will work
+            closely with our client and till they are 100% satisfied.
+          </p>
+          <br></br>
+          <h1 className="font-bold text-center max-w-lg text-[#80241b] pt-3">
+            About Allstate
+          </h1>
+          <p className="text-center max-w-lg text-[#ac1010]">
+            Established in 1971, we are family owned business that has been
+            providing outstanding flooring services to the Washington
+            metropolitan area for over 50 years. In September of 1971, Allstate
+            Building and Carpet Supplies, Inc. was established and moved into a
+            warehouse space in Alexandria, VA. Carolyn Pizzano became the owner
+            and ran the business from 1971-1983 when her daughter Lisa Paul
+            graduated from St. Leo college and took over the family business.
+            Now, Lisa and her son Jeremy continue to provide the DMV area with
+            quality flooring products.
+          </p>
+        </div>
+        <div
+          id="staff-photo-container"
+          className="flex flex-col justify-center md:flex-row align-middle my-auto flex-1"
+        >
+          <div className="flex flex-col mt-2 p-2 hover:brightness-50">
+            <Image
+              className="rounded-full  shadow-2xl pt-1"
+              src="/../public/LisaPaul.jpeg"
+              alt="picture1"
+              width={120}
+              height={120}
+            />
+            <p>Lisa Paul, President</p>
+          </div>
+          <div className="flex flex-col mt-2 p-2 hover:brightness-50">
+            <Image
+              className="rounded-full shadow-2xl pt-1"
+              src="/../public/JMP.jpeg"
+              alt="picture1"
+              width={160}
+              height={160}
+            />
+            <p>Jeremy Paul, Vice President</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
--- allstate-carpets-next/app/page.js
+++ allstate-carpets-next/app/page.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import './globals.css'
-
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-
+    <main className="bg-[#e3cba3] h-full">
+      <div>Gallery</div>
+      <div>testimonials</div>
+      <Footer />
     </main>
   )
 }

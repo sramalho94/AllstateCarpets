a793503ca03a8082e0f83b5f1ed68fd578a6c13a
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-06 12:21:48 -0500

    rendered Carousel component in home page

--- allstate-carpets-next/app/page.js
+++ allstate-carpets-next/app/page.js
@@ -2,13 +2,16 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import './globals.css'
 import Footer from '@/components/Footer'
+import Carousel from '@/components/Carousel'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="bg-[#e3cba3] h-full">
-      <div>Gallery</div>
+      <div>
+        <Carousel />
+      </div>
       <div>testimonials</div>
       <Footer />
     </main>

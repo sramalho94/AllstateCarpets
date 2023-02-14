16aa67ab93b4453765500ce52dabbb01c930b026
Jeff Carroll <jcarr048@gmail.com>
2023-02-14 15:21:39 -0500

    made ac logo clickable to navigate back to the home screen

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -5,7 +5,9 @@ const NavBar = () => {
   return (
     <div>
       <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#a4614c] border-b-2 border-[#80241b]">
+        <Link href="/">
         <Image src="/../public/aclogo.png " height={140} width={140} alt="logo" className='pl-3 pt-2'/>
+        </Link>
         <ul className="flex items-center">
           <li>
             <Link href="/" className="text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c]">

3d50d9e00ad498573481e1c4f0eb43d96c5fdbc8
Jeff Carroll <jcarr048@gmail.com>
2023-02-15 10:26:52 -0500

    made nav bar color lighter

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 const NavBar = () => {
   return (
     <div>
-      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#a4614c] border-b-2 border-[#80241b]">
+      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#cc7c64] border-b-2 border-[#80241b]">
         <Link href="/">
-        <Image src="/../public/aclogo.png " height={140} width={140} alt="logo" className='pl-3 pt-2'/>
+        <Image src="/../public/aclogo.png " height={140} width={140} alt="logo" className='pl-10 pt-2'/>
         </Link>
         <ul className="flex items-center">
           <li>

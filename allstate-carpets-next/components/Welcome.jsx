e72b0f06e0ebca48d3f08500c6ed4dd1c9dba1a4
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-15 12:20:00 -0500

    changes to text

--- allstate-carpets-next/components/Welcome.jsx
+++ allstate-carpets-next/components/Welcome.jsx
@@ -13,17 +13,27 @@ const Welcome = () => {
       className="flex flex-col md:flex-row-reverse w-screen bg-cover md:justify-between lg:justify-around bg-center"
     >
       <div className="flex flex-col my-10 mx-5 md:mt-80 justify-end">
-        <div className='bg-white rounded-2xl bg-opacity-50 px-5 py-2'>
-        <h1 className="font-bold text-center text-xl text-[#ac1010]">Welcome to Allstate Carpets</h1>
-        <p className="text-center max-w-lg text-[#ac1010] font-bold ">Let Us Floor You! Serving the DMV area since 1971
-        </p>
-        <div className='flex flex-row justify-center mb-5 mt-5'>
-        <Link href='/about' className="text-[#e3cba3] font-bold rounded p-2 bg-[#ac1010] ">About Us</Link>
-        </div>
+        <div className="bg-white rounded-2xl bg-opacity-50 px-5 py-2">
+          <h1 className="font-bold text-center text-xl text-[#ac1010]">
+            Welcome to Allstate Carpets
+          </h1>
+          <p className="text-center max-w-lg text-[#ac1010] font-bold ">
+            Let Us Floor You! Serving the commercial DMV area since 1971
+          </p>
+          <div className="flex flex-row justify-center mb-5 mt-5">
+            <Link
+              href="/contact"
+              className="text-[#e3cba3] font-bold rounded p-2 bg-[#ac1010] "
+            >
+              Free Consultations
+            </Link>
+          </div>
         </div>
       </div>
       <div className="flex flex-col  bg-white rounded-2xl my-10 mx-5 bg-opacity-50 md:mt-80">
-      <h1 className="font-bold text-xl text-[#ac1010] mt-5 ml-2 text-center">Services Provided</h1>
+        <h1 className="font-bold text-xl text-[#ac1010] mt-5 ml-2 text-center">
+          Commercial Services Provided
+        </h1>
         <ul className="grid grid-cols-2 list-disc  font-bold text-[#ac1010] my-8 px-4 py-4 justify-between">
           <li className="ml-2">Broadloom Carpet</li>
           <li className="ml-5">Broadloom over Pad</li>
@@ -36,7 +46,6 @@ const Welcome = () => {
           <li className="ml-2">Sheet Vinyl</li>
           <li className="ml-5">Sheet Welding</li>
           <li className="ml-2">Hardwood Floors</li>
-          <li className="ml-5">Free Estimates</li>
         </ul>
       </div>
     </div>

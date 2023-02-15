a3e2786747b3e6915b9df004dc02d8edcb9efc1c
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-15 12:20:21 -0500

    set up skeleton for contact component

---
+++ allstate-carpets-next/app/contact/page.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <div
+      id="contact-container"
+      className="bg-[#e3cba3] flex flex-col h-fit md:h-screen"
+    >
+      <h1>Contact Page</h1>
+    </div>
+  )
+}
+
+export default Contact

1e8776a55ea65776bf91d77cedefdfb7a6da8770
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-15 12:05:52 -0500

    added text to navbar

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -6,18 +6,38 @@ const NavBar = () => {
     <div>
       <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#cc7c64] border-b-2 border-[#80241b]">
         <Link href="/">
-        <Image src="/../public/aclogo.png " height={140} width={140} alt="logo" className='pl-10 pt-2'/>
+          <Image
+            src="/../public/aclogo.png "
+            height={140}
+            width={140}
+            alt="logo"
+            className="md:pl-10 pt-2"
+          />
         </Link>
+        <div className="md:flex md:flex-col my-auto hidden ">
+          <h1 className="font-bold text-l text-[#80241b] md:text-3xl text-scale">
+            Welcome to Allstate Carpets
+          </h1>
+          <h1 className="font-medium italic text-[#80241b]">
+            "Let Us Floor You!"
+          </h1>
+        </div>
         <ul className="flex items-center">
           <li>
-            <Link href="/" className="text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c]">
+            <Link
+              href="/"
+              className="text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c]"
+            >
               {' '}
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className='text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c]'>
-                About Us
+            <Link
+              href="/about"
+              className="text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c] text-scale"
+            >
+              About Us
             </Link>
           </li>
         </ul>

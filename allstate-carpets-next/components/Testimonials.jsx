e59ea211168d66f1786dec07894c6652298217a2
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-15 11:00:39 -0500

    added testimonial componet

---
+++ allstate-carpets-next/components/Testimonials.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div className="grid grid-cols-2 grid-rows-3 gap-4 mx-10 py-10">
+      <div
+        className="mx-auto
+      "
+      >
+        1
+      </div>
+      <div
+        className="mx-auto
+      "
+      >
+        2
+      </div>
+      <div
+        className="mx-auto
+      "
+      >
+        3
+      </div>
+      <div
+        className="mx-auto
+      "
+      >
+        4
+      </div>
+      <div
+        className="mx-auto
+      "
+      >
+        5
+      </div>
+      <div
+        className="mx-auto
+      "
+      >
+        6
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials

3e830c76ea1cd6c865860e1ff1d6fb14e66e6823
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-19 11:21:19 -0500

    first row of testimonials

--- allstate-carpets-next/components/Testimonials.jsx
+++ allstate-carpets-next/components/Testimonials.jsx
@@ -1,54 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import { fit } from 'sharp'
 
 const Testimonials = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-3 gap-4 mx-10 py-10 h-40">
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        <Image
-          src="/stockfloor2seg1.jpeg"
-          width={120}
-          height={120}
-          className="object-contain"
-        />
-      </div>
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        <Image
-          src="/stockfloor2seg2.jpeg"
-          width={120}
-          height={120}
-          className="h-fit w-fit"
-        />
-      </div>
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        3
-      </div>
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        4
-      </div>
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        5
-      </div>
-      <div
-        className="mx-auto h-1/3 col-span-1
-      "
-      >
-        6
+    <div className="flex flex-col mx-auto px-5">
+      <div className="flex flex-row justify-center ">
+        <div className="relative h-40 md:h-96 w-96  mr-5">
+          <Image src="/stockfloor2seg1.jpeg" fill />
+        </div>
+        <div className="relative h-40 md:h-96 w-96 ">
+          <Image src="/stockfloor2seg2.jpeg" fill />
+        </div>
       </div>
     </div>
   )

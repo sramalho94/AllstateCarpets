dd8775068455bf72db8d8db971a32aea8b2a9aa2
Stephan D Ramalho <sramalho@fordham.edu>
2023-02-22 13:17:24 -0500

    modified sizing

--- allstate-carpets-next/components/Testimonials.jsx
+++ allstate-carpets-next/components/Testimonials.jsx
@@ -4,50 +4,64 @@ import Image from 'next/image'
 const Testimonials = () => {
   return (
     <div className="flex flex-col mx-auto px-5">
+      <h1 className="text-4xl text-center font-bold pb-5 text-[#ac1010]">
+        Testimonials
+      </h1>
       <div className="flex flex-row justify-center pb-10">
-        <div className="relative h-40 md:h-96 w-96  mr-5">
-          <Image src="/stockfloor2seg1.jpeg" fill alt="floor1" />
+        <div className="relative h-40 md:h-96 w-96  mr-5 max-h-md">
+          <Image
+            src="/stockfloor2seg1.jpeg"
+            fill
+            alt="floor1"
+            className="object-fill"
+          />
         </div>
         <div className="flex flex-col relative h-40 md:h-96 w-96 overflow-x-auto justify-center ">
           <p className="whitespace-pre-wrap overflow-y-auto  text-center justify-center align-middle bg-[#ac1010] bg-opacity-80 font-bold shadow-xl rounded-xl p-5 text-[#e3cba3]">
-          &rsquo;As a property manager, I&apos;ve worked with many flooring companies
-            over the years, and Allstate Carpets is by far the best. They
-            recently installed luxury vinyl tile in one of my office buildings,
-            and the results were outstanding. The team was professional,
-            efficient, and went above and beyond to ensure the project was
-            completed on time and within budget. I highly recommend Allstate
-            Carpets for all your commercial flooring needs.&rsquo;
+            &rsquo;As a property manager, I&apos;ve worked with many flooring
+            companies over the years, and Allstate Carpets is by far the best.
+            They recently installed luxury vinyl tile in one of my office
+            buildings, and the results were outstanding. The team was
+            professional, efficient, and went above and beyond to ensure the
+            project was completed on time and within budget. I highly recommend
+            Allstate Carpets for all your commercial flooring needs.&rsquo;
           </p>
         </div>
       </div>
       <div className="flex flex-row justify-center pb-10">
         <div className="flex flex-col relative h-40 md:h-96 w-96 overflow-x-auto justify-center mr-5">
           <p className="whitespace-pre-wrap overflow-y-auto  text-center justify-center align-middle bg-[#ac1010] bg-opacity-80 font-bold shadow-xl rounded-xl p-5 text-[#e3cba3]">
-          &rsquo;We recently hired Allstate Carpets to install broadloom carpet in
-            our hotel&apos;s lobby and it was a great experience from start to
-            finish. The team was knowledgeable, courteous, and completed the job
-            with minimal disruption to our guests. The new carpet looks
-            beautiful and has received many compliments. We will definitely be
-            using Allstate Carpets for our future flooring projects.&rsquo;
+            &rsquo;We recently hired Allstate Carpets to install broadloom
+            carpet in our hotel&apos;s lobby and it was a great experience from
+            start to finish. The team was knowledgeable, courteous, and
+            completed the job with minimal disruption to our guests. The new
+            carpet looks beautiful and has received many compliments. We will
+            definitely be using Allstate Carpets for our future flooring
+            projects.&rsquo;
           </p>
         </div>
-        <div className="relative h-40 md:h-96 w-96 ">
+        <div className="relative h-40 md:h-96 w-96 max-h-md">
           <Image src="/stockfloor2seg2.jpeg" fill alt="floor2" />
         </div>
       </div>
       <div className="flex flex-row justify-center pb-10">
         <div className="relative h-40 md:h-96 w-96  mr-5">
-          <Image src="/stockfloor1.jpg" fill alt="floor3" />
+          <Image
+            src="/stockfloor1.jpg"
+            fill
+            alt="floor3"
+            className="object-cover"
+          />
         </div>
         <div className="flex flex-col relative h-40 md:h-96 w-96 overflow-x-auto justify-center ">
           <p className="whitespace-pre-wrap overflow-y-auto  text-center justify-center align-middle bg-[#ac1010] bg-opacity-80 font-bold shadow-xl rounded-xl p-5 text-[#e3cba3]">
-          &rsquo;Allstate Carpets is our go-to flooring company for our retail
+            &rsquo;Allstate Carpets is our go-to flooring company for our retail
             stores. They recently installed new sheet vinyl in one of our
             locations, and it was a seamless process from start to finish. The
             team was always on time, professional, and provided us with great
             recommendations on the best flooring options for our needs. We
-            couldn&apos;t be happier with the results and highly recommend Allstate
-            Carpets for all commercial flooring services.&rsquo;
+            couldn&apos;t be happier with the results and highly recommend
+            Allstate Carpets for all commercial flooring services.&rsquo;
           </p>
         </div>
       </div>

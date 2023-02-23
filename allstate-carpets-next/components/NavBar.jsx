4f589e48c31122f9122c0b4d0dde7ca203def20a
Jeff Carroll <jcarr048@gmail.com>
2023-02-23 00:07:52 -0500

    Made padding on bottom of navbar

--- allstate-carpets-next/components/NavBar.jsx
+++ allstate-carpets-next/components/NavBar.jsx
@@ -4,18 +4,18 @@ import Link from 'next/link'
 const NavBar = () => {
   return (
     <div>
-      <nav className="pb-4 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#cc7c64] border-b-2 border-[#80241b]">
+      <nav className="md:pb-10 flex justify-between bg-[#e3cba3] bg-gradient-to-r from-[#cc7c64] border-b-2 border-[#80241b]">
         <Link href="/">
           <Image
             src="/aclogo.png "
             height={120}
             width={120}
             alt="logo"
-            className="md:pl-10 pt-2 md:h-160 md:w-160"
+            className="md:pl-8 pt-2 md:h-160 md:w-160"
           />
         </Link>
         <div className="md:flex md:flex-col my-auto hidden ">
-          <h1 className="font-bold text-l text-[#80241b] md:text-5xl text-scale">
+          <h1 className="font-bold text-l text-[#80241b] md:text-5xl text-scale pt-4">
             Welcome to Allstate Carpets
           </h1>
           <h1 className="font-medium italic text-[#80241b]">
@@ -26,7 +26,7 @@ const NavBar = () => {
           <li>
             <Link
               href="/"
-              className="text-[#ac1010] font-bold rounded p-2 hover:bg-[#a4614c]"
+              className="text-[#ac1010] text-lg font-bold rounded p-2 hover:bg-[#a4614c]"
             >
               {' '}
               Home

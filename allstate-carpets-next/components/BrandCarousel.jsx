3fbdb15cd7565681d0bb6efe761e2f1f59beb029
Jeff Carroll <jcarr048@gmail.com>
2023-02-22 23:13:42 -0500

    Brand carousel is rendering, but needs to be configured so more brands are showing

--- allstate-carpets-next/components/BrandCarousel.jsx
+++ allstate-carpets-next/components/BrandCarousel.jsx
@@ -3,7 +3,6 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import { RxDotFilled } from 'react-icons/rx'
 import { useSwipeable } from 'react-swipeable'
 
 const BrandCarousel = () => {
@@ -14,6 +13,33 @@ const BrandCarousel = () => {
     {
       url: '/HBW.png'
     },
+    {
+      url: '/Patner.png'
+    },
+    {
+      url: '/Range.png'
+    },
+    {
+      url: '/JLL.png'
+    },
+    {
+      url: '/Teel.png'
+    },
+    { 
+      url: '/Penzance.png'
+    },
+    {
+      url: '/Simpson.png'
+    },
+    {
+      url: '/MetroMGMT.png'
+    },
+    {
+      url: '/Jstreet.png'
+    },
+    {
+      url: '/Boland.png'
+    }
   ]
 
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -35,13 +61,13 @@ const BrandCarousel = () => {
   }
 
   return (
-    <div className="max-w-[1440px] h-[full] md:w-[780px] m-auto py-16 px-4 relative group">
-      <h1 className="text-4xl font-bold text-center mb-7 text-[#ac1010]">
+    <div className="max-w-[1440px] h-[full] md:w-[780px] m-auto py-16 px-4 relative group ">
+      <h1 className="text-4xl font-bold text-center mb-2 text-[#ac1010]">
         Partners
       </h1>
 
       <div
-        className="w-full md:w-[780px] h-full rounded-2xl bg-center bg-cover duration-500"
+        className="w-full md:w-[780px] h-full rounded-2xl bg-center bg-cover duration-500 bg-white px-10 py-5"
         id="1"
         {...useSwipeable({
           onSwipedLeft: () => nextSlide(),
@@ -50,18 +76,18 @@ const BrandCarousel = () => {
       >
         <Image
           alt="image1"
-          width={100}
-          height={100}
+          width={400}
+          height={400}
           src={`${slides[currentIndex].url}`}
-          className="w-[780px] h-full rounded-2xl bg-center bg-cover duration-500"
+          className="w-[100px] h-[60px] rounded-2xl bg-center bg-cover duration-500"
         />
         {/* Left Arrow */}
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-1 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+          <BsChevronCompactLeft onClick={prevSlide} size={10} />
         </div>
         {/* Right Arrow */}
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-1 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <BsChevronCompactRight size={30} onClick={nextSlide} />
+          <BsChevronCompactRight size={10} onClick={nextSlide} />
         </div>
       </div>
 
@@ -72,7 +98,6 @@ const BrandCarousel = () => {
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
           >
-            <RxDotFilled />
           </div>
         ))}
       </div>
